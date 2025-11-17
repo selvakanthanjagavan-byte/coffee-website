1769fee0c3bbdbf5bb414adcfc6afdbd0702d259
selvakanthanjagavan-byte <selvakanthanjagavan@gmail.com>
2025-11-17 16:49:29 +0530

    coffeewebsite

---
+++ script.js
@@ -0,0 +1,40 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Initialize Swiper
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 25,
+
+  // Pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
